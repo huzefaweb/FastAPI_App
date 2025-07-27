89dcfab63dc5e7de4bd7b2c6845c94df3d721af2
huzefaweb <huzefa.ahmed.web@gmail.com>
2025-07-27 20:09:42 +0500

    chore: initial commit

--- backend/api/index.py
+++
@@ -1,6 +0,0 @@
-# backend/api/index.py
-from app.main import app as fastapi_app
-from mangum import Mangum
-
-# Mangum wraps your FastAPI app into a lambda‐style handler
-handler = Mangum(fastapi_app)
